0099ff89e98f0baf471945242f5134d5ddea1848
czM1K3 <misamadera1@gmail.com>
2021-12-06 15:49:00 +0100

    Initial commit

---
+++ app/src/main/java/cz/madsoft/tahak/MainActivity.java
@@ -0,0 +1,23 @@
+package cz.madsoft.tahak;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import cz.madsoft.tahak.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mTextView = binding.text;
+    }
+}
